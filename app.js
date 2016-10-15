7eac55c6209f36862b80e79248bd5340a4692e7d
root <root@localhost.localdomain>
2016-10-16 02:21:35 +0700

    Multipart Support

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboy = require('connect-busboy'); 
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
+app.use(busboy({ immediate: true }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/target.js
+++ routes/target.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var targetModel = require('../model/target');
+var fs = require('fs-extra');
+var csv = require("fast-csv");
 
 router.get('/', function(req, res, next) {
-	res.render('target/index', {});
+	targetModel.find({},function(err,data){
+		if (err) throw err;
+		else res.render('target/index', {data:data});
+	});
 });
 
 router.get('/add', function(req, res, next) {
@@ -11,14 +16,53 @@ router.get('/add', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
-  console.log(req.body);
-  var t = new targetModel(req.body);
-  t.save();
-  res.redirect('/target');
+  	var fstream;
+	var data = {};
+	req.busboy.on('field', function(fieldname, val) {
+	    data[fieldname] = val;
+	});
+
+	req.pipe(req.busboy);
+	req.busboy.on('file', function (fieldname, file, filename) {
+		console.log(fieldname);
+		if (filename != '') {
+		    console.log("Uploading: " + filename);
+
+		    //Path where image will be uploaded
+		    var loc = __dirname + '/../public/uploads/' + filename;
+		    fstream = fs.createWriteStream(loc);
+		    file.pipe(fstream);
+		    fstream.on('close', function () {    
+		        console.log("Upload Finished of " + filename);
+		        csv
+				 .fromPath(loc)
+				 .on("data", function(data){
+				     console.log(data);
+				 })
+				 .on("end", function(){
+				     console.log("done");
+				     res.redirect('/target');
+				 });
+		    });
+	    } else {
+	    	new targetModel(data).save();
+	    	res.redirect('/target');
+	    }
+	});
 });
 
-router.get('/delete/{id}', function(req, res, next) {
-  res.render('target', {});
+router.get('/delete/:id', function(req, res, next) {
+  targetModel.remove({_id: req.params.id}, function(err){
+  	if (err) throw err;
+  	else res.redirect('/target');
+  });
+});
+
+router.get('/deleteall', function(req, res, next) {
+  targetModel.remove({}, function(err){
+  	if (err) throw err;
+  	else res.redirect('/target');
+  });
 });
 
 module.exports = router;
